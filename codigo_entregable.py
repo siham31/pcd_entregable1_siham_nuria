c649d6023cc7d268f403f38904558c882a2cf38f
nj-xuzhu-um-es <nj-xuzhu-um-es>
2026-03-14 01:02:15 +0100

    Implementación básica completada: ajustes estructura de datos de campo de Pieza

--- codigo_entregable.py
+++ codigo_entregable.py
@@ -14,23 +14,46 @@ class EClase(Enum):
 
 
 
-# implemetamos las excepciones 
+# Implemetamos las excepciones 
+
+# Excepciones de Stock
 class StockInsuficienteError(Exception):
     """ Se lanza cuando no hay suficientes stock de una pieza para atender a un pedido"""
     pass
 
+class StockNoEncontradoError(Exception):
+    """Se lanza cuando se busca el stock que no existe en el almacen"""
+    pass
+
+# Excepciones de Repuesto
 class RepuestoNoEncontradoError(Exception):
     """ Se lanza cuando se busca una pieza que no existe en el catálogo
     """
     pass
 
+class RepuestoVacioError(Exception):
+    """Se lanza cuando el listado de respuestos está vacío"""
+    pass 
+
+class RepuestoDuplicadoError(Exception):
+    """Se lanza cuandi se intenta añadir una pieza (repuesto) ya existe en el almacen
+    seleccionado"""
+    pass
+
+# Excepciones de Almacen
 class AlmacenNoEncontradoError(Exception):
-    """"Se lamza cuando se busca un almacen que no existe en el listado de almacenes"""
+    """"Se lanza cuando se busca un almacen que no existe en el listado de almacenes"""
+    pass
 
 class AlmacenDuplicadoError(Exception):
     """Se lanza cuando se intenta añadir un almacén que ya existe."""
     pass
 
+class AlmacenVacioError(Exception):
+    """Se lanza cuando el listado de almacenes está vacío"""
+    pass
+
+# Excepciones de Nave 
 class NaveNoEncontradaError(Exception):
     """Se lanza cuando se intenta operar sobre una nave no registrada."""
     pass
@@ -71,33 +94,42 @@ class Almacen:
     def __init__(self, nombre, ub_localizacion):
         self.nombre = nombre
         self.loc = ub_localizacion
-        self.cat_rep = dict[str,Pieza] = {}
+        self.cat_rep = []
     
     def devuelveAlmacen(self):
         return f"Nombre: {self.nombre} \tLocalización: {self.loc}"
     
     def anyadirStock(self, nombre, cantidad):
+        if cantidad < 0:
+            raise ValueError("La cantidad introducida no es correcta")
+        
         for s in self.cat_rep:
-            if (s == nombre):
-                self.cat_rep[nombre] += cantidad
+            if (s.nombre == nombre):
+                s.cantidad += cantidad
+                print("La cantidad ha sido añadido con éxito")
                 return
-        print(f"No existe la pieza {nombre}")
+        raise StockNoEncontradoError(f"No existe la pieza {nombre}")
     
-    def anyadir_pieza(self,pieza):
-        if pieza.nombre in self.cat_rep:
-            print(f"almacen {self.nombre}  La pieza :{pieza.nombre} ya existe en el catalogo")
-        else:
-            self.cat_rep[pieza.nombre]=pieza
-            print(f"Almacen {self.nombre} pieza{pieza.nombre} añadida al catalogo")
+    def anyadir_pieza(self,pieza, proveedor, precio, cantidad):
+        for p in self.cat_rep:
+            if p.nombre == pieza:
+                print(f"Almacen {self.nombre}: pieza :{pieza.nombre} ya existe en el catalogo")
+            else:
+                self.cat_rep.append(Pieza(pieza, proveedor, precio, cantidad))
+                print(f"Almacen {self.nombre} pieza{pieza.nombre} añadida al catalogo")
 
     def retirar_repuesto(self,nombre,cantidad):
         """   retira unidades de una pieza y devuelve el coste total"""
         if nombre not in self.cat_rep:
-            raise RepuestoNoEncontradoError(f"ALmacen {self.nombre} pieza {nombre} no encontrada")
-        pieza = self.cat_rep[nombre]
-        pieza.retirar_stock(cantidad)
-        coste = pieza.precio * cantidad
-        return coste
+            raise RepuestoNoEncontradoError(f"En el almacen {self.nombre}: pieza {nombre} no encontrada")
+        
+        for p in self.cat_rep:
+            if p.nombre == nombre:
+                pieza = p.nombre
+                pieza.cantidad -= cantidad
+                coste = pieza.precio * cantidad
+                return coste
+        raise RepuestoNoEncontradoError(f"En el almacen {self.nombre}: pieza {nombre} no se ha encontrado")
 
 class Estacion_Espacial(Nave,Uni_Comb):
     def __init__(self, nombre, tripulacion, ubicacion, pasaje, id_combate, clave_transmision):
@@ -158,46 +190,113 @@ class Pieza:
     
     def get_Pieza(self):
         return self.nombre, self.precio  
-          
+
     def get_cantidad(self) -> int:
         return self.cant_disp
     
     def set_cantidad(self,cantidad:int):
-        if cantidad <0:
+        if cantidad < 0:
             raise ValueError("la cantidad no puede ser negativa")
         self.cant_disp = cantidad
     
 
+# Clase Milmperio
 class Milmprerio:
     def __init__(self):
-        self.repuestos = []
-        self.almacenes = []
+        self._repuestos = []
+        self._almacenes = []
     
-    def add_Almacen(self, nombre, localizacion):
-        for a in self.almacenes:
+    # Almacen
+    def addAlmacen(self, nombre, localizacion):
+        """Añadir el almacen (nombre) con una ubicacion (localizacion) en el listado"""
+        for a in self._almacenes:
             if a == nombre:
                 raise AlmacenDuplicadoError(f"El almacen con nombre {a} ya está registrado en el sistema")
-        self.almacenes.append(Almacen(nombre, localizacion))
+        self._almacenes.append(Almacen(nombre, localizacion))
         
-        
-    def get_almacen(self,nombre):
-        for a in self.almacenes:
+    def getAlmacen(self,nombre):
+        """Ver el almacen nombre si esta en el sistema"""
+        for a in self._almacenes:
             if a.nombre == nombre :  # comparamos que los nombres coinciden
                 return a
-        raise RepuestoNoEncontradoError(f"Almacen {nombre} no encontrado")
+        raise AlmacenDuplicadoError(f"Almacen {nombre} no encontrado")
     
     def listarAlmacen(self):
-        if len(self.almacenes) == 0:
+        """Listar todos los almacenes registrados en el sistema"""
+        if len(self._almacenes) == 0:
             print(f"No hay almacenes registrados en el sistema")
             return
-        for a in self.almacenes:
+        for a in self._almacenes:
             print(a.devuelveAlmacen)
     
+    def quitarAlmacen(self, nombre):
+        """Eliminar el almacen nombre del listado self._almacenes"""
+        for a in self._almacenes:
+            if a.nombre == nombre:
+                self._almacenes.remove(nombre)
+                return
+        raise AlmacenNoEncontradoError(f"El almacen {nombre} no se encuentra registrado")
+    
+    # Repuesto
+    def addRepuesto(self, nombre, proveedor, precio, cantidad, almacen):
+        """Añadir la pieza (nombre) con cantidad (cantidad) en el almacen (almacen)"""
+        if cantidad < 0:
+            raise ValueError(f"La cantidad introducida debe ser positiva")
+        
+        for a in self._almacenes:
+            if a.nombre == almacen:
+                catalogo = a.cat_rep
+                
+                for p in catalogo.keys():
+                    if p.nombre == nombre:
+                        raise RepuestoDuplicadoError(f"La pieza {nombre} ya existe en el almacen {almacen}")
+                
+                catalogo.append(Pieza(nombre, proveedor, precio, cantidad))
+                print(f"La pieza {nombre} ha sido añadido con éxito en el almacen {almacen}")
+                return
+        raise AlmacenNoEncontradoError(f"El almacen {almacen} no se encuentra registrado")
+    
+    def getRepuesto(self, nombre, almacen):
+        """Ver si la pieza (nombre) en el almacen (almacen)"""
+        for a in self._almacenes:
+            if a.nombre == almacen:
+                catalogo = a.cat_rep 
+                
+                for p in catalogo:
+                    if p.nombre == nombre:
+                        print(f"La pieza {nombre} está en el almacen {almacen}")
+                        return
+                raise RepuestoNoEncontradoError(f"La pieza {nombre} no se encuentra en el almacen {almacen}")
+        raise AlmacenNoEncontradoError(f"El almacen {almacen} no se encuentra registrado")
+    
     def listarRepuestos(self):
-        if len(self.repuestos) == 0:
-            print("No hay piezas registradas en el sistema")
-            return
-        for p in self.repuestos:
+        """Listar todas las piezas de repuestos registrados en el sistema"""
+        if len(self._repuestos) == 0:
+            raise ValueError(f"No hay piezas registradas en el sistema")
+            #print("No hay piezas registradas en el sistema")
+            #return
+        for p in self._repuestos:
             print(p.devuelvePieza)
     
-    def 
+    def quitarRepuesto(self, nombre, almacen): 
+        """Quitar la pieza (nombre) del almacen (almacen)"""
+        for a in self._almacenes:
+            if a.nombre == almacen:
+                catalogo = a.cat_rep
+                
+                for p in catalogo:
+                    if p.nombre == nombre:
+                        catalogo.remove(nombre)
+                        print(f"Eliminado con éxito la pieza {nombre} del almacen {almacen}")
+                        return
+                raise RepuestoNoEncontradoError(f"La pieza {nombre} no se encuentra en el almacen {almacen}")
+        raise AlmacenNoEncontradoError(f"El almacen {almacen} no se encuentra registrado en el sistema")
+    
+    # Stock
+    def listarStocks(self, almacen):
+        for a in self._almacenes:
+            if a.nombre == almacen:
+                print(f"Listado de stock en el almacen {almacen}")
+                for p in a.cat_rep:
+                    print(f"-\t{p}")
+            raise AlmacenNoEncontradoError(f"El almacen {almacen} no se encuentra registardo en el sistema")

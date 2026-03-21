904d10808d12e97397e5b633026d6fdbfd138e5c
siham31 <siham60501@gmail.com>
2026-03-21 22:28:22 +0100

    repasando el código

--- codigo_entregable.py
+++ codigo_entregable.py
@@ -212,7 +212,7 @@ class Caza_Estelar(Nave,Uni_Comb):
         			Id Combate: {self.id_combate}\tClave Transmisión: {self.clave_trans}"""
     
     def devuelveDotacion(self):
-        return f"Dotacion de {self.nombre}: {self.dotacion}"
+        return f"DotaciÓn de {self.nombre}: {self.dotacion}"
 
 class Pieza:
     """    representa una pieza de respuesto almacenada en un almacen """
